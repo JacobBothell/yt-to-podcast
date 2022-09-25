ddabcb6f68201cb69d10cf2c5dcc1cd99a7e9e08
Jacob Bothell <jacob20140273@gmail.com>
2022-09-25 16:23:05 -0500

    download video and split audio into chapters

--- get_youtube_data.js
+++ get_youtube_data.js
@@ -1,5 +1,8 @@
 import {google} from 'googleapis';
 import ytData from './yt_data.js';
+import youtubeDlExec from 'youtube-dl-exec';
+import fs from 'fs';
+//import { Puppeteer } from 'puppeteer';
 
 var plID = ytData.playlist;
 var chanID = ytData.chan_id;
@@ -20,7 +23,6 @@ function getPlaylistItems(playlistID) {
             playlistId:plID,
             maxResults:50
         }).catch((err) => {reject(err)})
-        console.log(res.data);
         totalResults += res.data.items.length;
         pageData.nextPageToken = res.data.nextPageToken;
         pageData.pageInfo = res.data.pageInfo;
@@ -36,9 +38,62 @@ function next(){
             maxResults:50,
             pageToken: pageData.nextPageToken
         })
-        console.log(res.data)
     })
 }
 
-getPlaylistItems(plID)
-    .then((ret) => next())
+function downloadYTVids(){
+    return new Promise(async (resolve, reject) => {
+        //get page
+        let YTPage = await getPlaylistItems(plID)
+        //iterate through page
+        //for(let video in YTPage.items){
+            let subprocess = youtubeDlExec.exec("https://www.youtube.com/watch?v="+YTPage.items[0].contentDetails.videoId,{
+                //dumpSingleJson: true,
+                noCheckCertificates: true,
+                noWarnings: true,
+                preferFreeFormats: true,
+                addHeader: [
+                  'referer:youtube.com',
+                  'user-agent:googlebot'
+                ],
+                extractAudio: true,
+                audioFormat: "mp3",
+                splitChapters: true,
+                ffmpegLocation: "C:\\ffmpeg\\bin"
+              
+            })
+
+        //subprocess.stdout.pipe(fs.createWriteStream('stdout.txt'))
+        subprocess.stdout.on('data', (data) => {
+            //parse output of yt-dlp
+            let stringData = data.toString()
+            //look for chapter slitter output
+            if(stringData.split(" ")[0] == "[SplitChapters]"){
+                let lines = stringData.split(/\r?\n/)
+                for(let line in lines){
+                    //look for chapter name
+                    let dashSplit = lines[line].split(" - ")
+                    if(dashSplit[dashSplit.length - 1].toUpperCase().includes(ytData.chapter.toUpperCase())){
+                        //split out file name
+                        let destSplit = lines[line].split("; Destination: ")
+                        console.log(destSplit[1])
+                    }
+                }
+            }
+        });
+        subprocess.stdout.on('error', (data) => {console.log(data)})
+        //subprocess.stderr.pipe(fs.createWriteStream('stderr.txt'))
+
+        //subprocess.on('exit', function(){
+            //uploadToAnchor("")
+        //})
+
+        setTimeout(subprocess.cancel, 30000)
+        //}
+        //get next page
+        //getPlaylistItems(plID)
+        //    .then((ret) => next())
+    })
+}
+
+downloadYTVids()
